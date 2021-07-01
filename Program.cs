1aa08b86a9c724fab324b8f5152233e5f88fb8b7
Damian <65552465+damian568@users.noreply.github.com>
2021-07-01 11:32:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Hotel
+{
+    class Program
+    {
+        public static void printQuestions()
+        {
+            Console.WriteLine("Please select what you want to do:");
+            Console.WriteLine("1 - Make a reservation;");
+            Console.WriteLine("2- List free rooms;");
+            Console.WriteLine("3 - Checkout room;");
+            Console.WriteLine("4 - Stats;");
+            Console.WriteLine("5 - Find a room;");
+            Console.WriteLine("6 - Update a room;");
+            Console.WriteLine("7 - Exit;");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Choose number: ");
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                printQuestions();
+
+                int number = Int32.Parse(Console.ReadLine());
+
+                if(number == 1)
+                {
+
+                }
+                else if (number == 2)
+                {
+
+                }
+                else if (number == 3)
+                {
+
+                }
+                else if (number == 4)
+                {
+
+                }
+                else if (number == 5)
+                {
+
+                }
+                else if (number == 6)
+                {
+
+                }
+                else if (number == 7)
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
